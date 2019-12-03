fc5d1a7925f9393236b7be0b0e89a1ce52c87f38
Faizun-Faria <34593750+Faizun-Faria@users.noreply.github.com>
2019-12-03 12:24:43 +0600

    Add files via upload

--- SFAC/src/sfac/AlumniDashboardController.java
+++ SFAC/src/sfac/AlumniDashboardController.java
@@ -100,78 +100,67 @@ public class AlumniDashboardController implements Initializable {
                 rowCount++;
             }
 
-            /*
-            for (int i = rowCount; i > 0; i--) {
-                query = "SELECT ORG_NAME FROM POST WHERE ROW_NUMBER() = '" + i + "'";
-                statement = connection.createStatement();
-                rs = statement.executeQuery(query);
-            }
-            rs.close();
-            statement.close();
-            connection.close(); */
         } catch (SQLException e) {
         }
 
-        
-
         // adding grid pane too
-        for (int i = rowCount; i > 0; i--) {
-            GridPane grid = new GridPane();
-            grid.setPadding(new Insets(35, 100, 10, 10));
-            grid.setMinSize(200, 150);
-            grid.setHgap(5);
-            grid.setVgap(5);
+        PreparedStatement ps;
+        try {
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/myDatabase", "app", "app");
+            query = "SELECT * FROM POST ";
+            ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            int i = 0;
+            while (rs.next()) {
+                GridPane grid = new GridPane();
+                grid.setPadding(new Insets(35, 100, 10, 10));
+                grid.setMinSize(200, 150);
+                grid.setHgap(5);
+                grid.setVgap(5);
+                org = rs.getString(1);
+                posi = rs.getString(2);
+                descr = rs.getString(3);
+                ti = rs.getString(4);
+                da = rs.getString(5);
+                tiSt = rs.getString(6);
+                alu = rs.getString(7);
 
-            PreparedStatement ps;
-            try {
-                connection = DriverManager.getConnection("jdbc:derby://localhost:1527/myDatabase", "app", "app");
-                query = "SELECT * FROM POST WHERE ROW_NUMBER() = '" + i + "'";
-                ps = connection.prepareStatement(query);
-                ResultSet rs = ps.executeQuery();
-                
-                    System.out.println("Fifi");
-                if (rs.next()) {
-                    org = rs.getString(1);
-                    posi = rs.getString(2);
-                    descr = rs.getString(3);
-                    ti = rs.getString(4);
-                    da = rs.getString(5);
-                    tiSt = rs.getString(6);
-                    alu = rs.getString(7);
+                Text username = new Text(alu);
+                username.setStyle("-fx-font-weight: bold");
+                grid.add(username, 0, 0);
+
+                Text time = new Text(tiSt);
+                time.setStyle("-fx-font-style: italic");
+                grid.add(time, 0, 1);
+
+                Text oName = new Text(org);
+                oName.setStyle("-fx-font-weight: regular");
+                grid.add(oName, 0, 2);
+
+                Text posit = new Text(posi);
+                posit.setStyle("-fx-font-weight: regular");
+                grid.add(posit, 0, 3);
+
+                Text post = new Text(descr);
+                post.setStyle("-fx-font-weight: regular");
+                grid.add(post, 0, 4);
+
+                if (i % 2 == 0) {
+                    grid.setStyle("-fx-background-color: #d1c4e9;");
+                } else {
+                    grid.setStyle("-fx-background-color: #90caf9;");
                 }
-            } catch (SQLException ex) {
-                
-                    System.out.println("fafff");
+
+                vb.getChildren().add(grid);
+                i++;
             }
-
-            Text username = new Text(alu);
-            username.setStyle("-fx-font-weight: bold");
-            grid.add(username, 0, 0);
-
-            Text time = new Text(tiSt);
-            time.setStyle("-fx-font-style: italic");
-            grid.add(time, 0, 1);
-
-            Text oName = new Text(org);
-            oName.setStyle("-fx-font-weight: regular");
-            grid.add(oName, 0, 2);
-
-            Text posit = new Text(posi);
-            posit.setStyle("-fx-font-weight: regular");
-            grid.add(posit, 0, 3);
-
-            Text post = new Text(descr);
-            post.setStyle("-fx-font-weight: regular");
-            grid.add(post, 0, 4);
-
-            if (i % 2 == 0) {
-                grid.setStyle("-fx-background-color: #d1c4e9;");
-            } else {
-                grid.setStyle("-fx-background-color: #90caf9;");
-            }
-
-            vb.getChildren().add(grid);
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException ex) {
+            
         }
+
         sp.setContent(vb);
         sp.setFitToHeight(true);
         sp.setPrefSize(447, 364);
